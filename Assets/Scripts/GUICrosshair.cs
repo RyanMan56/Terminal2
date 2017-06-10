fdb81b3f9515ba5791f691ec53d4bad8c0df1e5f
Ryan Jones <Ryan Jones>
2017-06-11 00:30:34 +0100

    First commit

---
+++ Assets/Scripts/GUICrosshair.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUICrosshair : MonoBehaviour {
+    private Texture2D crosshair, outerCrosshair;
+    private Vector2 lastWindowSize;
+    private Rect crosshairRect;//, outerCrosshairRect;
+
+	// Use this for initialization
+	void Start () {
+        crosshair = new Texture2D(2, 2);
+        //outerCrosshair = new Texture2D(3, 3);
+
+        //for (int i = 0; i < outerCrosshair.width; i++)
+        //    for (int j = 0; j < outerCrosshair.height; j++)
+        //        outerCrosshair.SetPixel(i, j, Color.black);
+        
+        lastWindowSize = new Vector2(Screen.width, Screen.height);
+        CalculateRect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lastWindowSize.x != Screen.width || Screen.height != Screen.height)
+        {
+            CalculateRect();
+        }
+    }
+
+    void CalculateRect() {
+        lastWindowSize = new Vector2(Screen.width, Screen.height);
+        crosshairRect = new Rect((lastWindowSize.x - crosshair.width) / 2.0f, (lastWindowSize.y - crosshair.height) / 2.0f, crosshair.width, crosshair.height);
+        //outerCrosshairRect = new Rect((lastWindowSize.x - outerCrosshair.width) / 2.0f, (lastWindowSize.y - outerCrosshair.height) / 2.0f, outerCrosshair.width, outerCrosshair.height);
+    }
+
+    void OnGUI() {
+        GUI.DrawTexture(crosshairRect, crosshair);
+        //GUI.DrawTexture(new Rect(Input.mousePosition, new Vector2(2, 2)), crosshair);
+        //GUI.DrawTexture(outerCrosshairRect, outerCrosshair);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private float speed = 6.0f;
+    private float mouseSensitivity = 2.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private float verticalVelocity = 0f;
+    private float verticalRotation = 0;
+    private float jumpSpeed = 5.0f;
+    public bool canMove;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float yAxisRot = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, yAxisRot, 0);
+
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+        if (canMove)
+        {
+            if (controller.isGrounded && Input.GetButton("Jump"))
+                verticalVelocity = jumpSpeed;
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, verticalVelocity, Input.GetAxis("Vertical") * speed);
+            moveDirection = transform.TransformDirection(moveDirection);
+            //        moveDirection *= speed;
+
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+	}
+
+    public float pushPower = 2.0F;
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+            return;
+
+        if (hit.moveDirection.y < -0.3F)
+            return;
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * pushPower;
+    }
+}
