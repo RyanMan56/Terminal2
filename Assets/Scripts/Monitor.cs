d40c4837035544e9f909fc2dd705bd7746182587
Ryan Jones <Ryan Jones>
2017-06-14 23:51:47 +0100

    Started work on writing to screen

---
+++ Assets/Scripts/Monitor.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Monitor : MonoBehaviour {
+	public int width = 88;
+	public int height = 24;
+
+    private GameObject _screenTextObj;
+    private Text _screenText;
+    private GameObject _player;
+    private PlayerController _playerController;
+    private Collider _screenCollider;
+
+    private string _currentCmd;
+    private List<List<char>> _textArray, _lastTextArray;
+
+	// Use this for initialization
+	void Start () {
+        _screenTextObj = transform.Find("Object/Screen/Canvas/ScreenText").gameObject;
+        _screenText = _screenTextObj.GetComponent<Text>();
+        _player = GameObject.Find("Player");
+        _playerController = _player.GetComponent<PlayerController>();
+        _screenCollider = GameObject.Find("Object/Screen").GetComponent<BoxCollider>();
+
+        _textArray = new List<List<char>>();
+        _lastTextArray = new List<List<char>>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckTouch();
+        if (!_playerController.canMove)
+        {
+            TextInput();
+        }
+        UpdateScreen();
+	}
+
+    void CheckTouch()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector2((Screen.width - 1) / 2, (Screen.height - 1) / 2));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1.5f))
+            {
+                if (hit.collider.Equals(_screenCollider)) _playerController.canMove = !_playerController.canMove;
+            }                       
+        }
+    }
+
+    void TextInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Returned value: " + _currentCmd);
+            _currentCmd = "";
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+
+        }
+        else if (Input.inputString != "")
+        {
+            Debug.Log(Input.inputString);
+            _currentCmd += Input.inputString;
+            AddTextValue(Input.inputString[0]);
+        }
+    }
+    
+    void UpdateScreen()
+    {
+        if (!_lastTextArray.Equals(_textArray)) {
+            Debug.Log("Helooooooooo");
+            _screenText.text = GridToString(_textArray);
+            _lastTextArray = _textArray;
+        }
+    }
+
+    string GridToString(List<List<char>> chars2d)
+    {
+        string text = "";
+        foreach (List<char> chars in chars2d)
+        {
+            text += chars;
+            if (chars.Count < width) text += '\n';
+        }
+        return text;
+    }
+
+    void AddTextValue(char val)
+    {
+        if (_textArray.Count.Equals(0) || _textArray[_textArray.Count - 1].Count.Equals(width - 1))
+        {
+            _textArray.Add(new List<char>() { val });
+        }
+        else
+        {
+            _textArray[_textArray.Count - 1].Add(val);
+        }
+    }    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-    private float speed = 6.0f;
+    private float defaultSpeed = 6.0f, speed = 6.0f;
     private float mouseSensitivity = 2.0f;
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
@@ -19,26 +19,8 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float yAxisRot = Input.GetAxis("Mouse X") * mouseSensitivity;
-        transform.Rotate(0, yAxisRot, 0);
-
-        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
-        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
-
-        verticalVelocity += Physics.gravity.y * Time.deltaTime;
-
-        if (canMove)
-        {
-            if (controller.isGrounded && Input.GetButton("Jump"))
-                verticalVelocity = jumpSpeed;
-
-            moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, verticalVelocity, Input.GetAxis("Vertical") * speed);
-            moveDirection = transform.TransformDirection(moveDirection);
-            //        moveDirection *= speed;
-
-            controller.Move(moveDirection * Time.deltaTime);
-        }
+        Move();
+        CheckMoveExit();
 	}
 
     public float pushPower = 2.0F;
@@ -53,5 +35,37 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
         body.velocity = pushDir * pushPower;
+    }
+
+    void Move()
+    {
+        float yAxisRot = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, yAxisRot, 0);
+
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+        if (controller.isGrounded && Input.GetButton("Jump") && canMove)
+        {
+            verticalVelocity = jumpSpeed;
+        }
+
+        speed = canMove ? defaultSpeed : speed * 0.9f;
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, verticalVelocity, Input.GetAxis("Vertical") * speed);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void CheckMoveExit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            canMove = true;
+        }
     }
 }
