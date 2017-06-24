4738b99e27c4f5e02ec72c2620a40c7e141188b4
Ryan <ryanman56@gmail.com>
2017-06-24 21:36:33 +0100

    Started on arrow keys pressed

--- Assets/Scripts/Monitor.cs
+++ Assets/Scripts/Monitor.cs
@@ -3,10 +3,12 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Monitor : MonoBehaviour {
-	public int width = 88;
-	public int height = 24;
+public class Monitor : MonoBehaviour
+{
+    public int width = 88;
+    public int height = 24;
     public Vector2 CurrentPosition;
+    public int CurrentPositionInCmd = 0;
 
     private GameObject _screenTextObj;
     private Text _screenText;
@@ -16,10 +18,11 @@ public class Monitor : MonoBehaviour {
     private ScreenCursor _cursorScript;
 
     private string _currentCmd = "";
-    private List<List<char>> _textArray, _lastTextArray;    
+    private List<List<char>> _textArray, _lastTextArray;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         _screenTextObj = transform.Find("Object/Screen/Canvas/ScreenText").gameObject;
         _screenText = _screenTextObj.GetComponent<Text>();
         _player = GameObject.Find("Player");
@@ -30,18 +33,27 @@ public class Monitor : MonoBehaviour {
         _textArray = new List<List<char>>();
         _lastTextArray = new List<List<char>>();
         CurrentPosition = new Vector2(0, 0);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log(CurrentPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         CheckTouch();
         if (!_playerController.canMove)
         {
             TextInput();
         }
         UpdateScreen();
-	}
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey && ((e.keyCode == KeyCode.LeftArrow) || e.keyCode == KeyCode.RightArrow))
+        {
+            ArrowPressed(e.keyCode); // Needs to be done here unfortunately, Input.inputString only supports ASCII characters
+        }
+    }
 
     void CheckTouch()
     {
@@ -52,7 +64,7 @@ public class Monitor : MonoBehaviour {
             if (Physics.Raycast(ray, out hit, 1.5f))
             {
                 if (hit.collider.Equals(_screenCollider)) _playerController.canMove = !_playerController.canMove;
-            }                       
+            }
         }
     }
 
@@ -61,29 +73,31 @@ public class Monitor : MonoBehaviour {
         string inputString = Input.inputString;
         if (inputString != "")
         {
+            Debug.Log(inputString);
             foreach (KeyCode c in inputString)
             {
                 _cursorScript.ResetTimer();
-                if (c.Equals(KeyCode.Return))
+                switch (c)
                 {
-                    ReturnPressed();
+                    case KeyCode.Return:
+                        ReturnPressed();
+                        break;
+                    case KeyCode.Backspace:
+                        BackspacePressed();
+                        break;
+                    default:
+                        _currentCmd += Input.inputString;
+                        AddTextValue((char)c);
+                        break;
                 }
-                else if (c.Equals(KeyCode.Backspace))
-                {
-                    BackspacePressed();
-                }
-                else
-                {
-                    _currentCmd += Input.inputString;
-                    AddTextValue((char)c);
-                }                
             }
         }
     }
-    
+
     void UpdateScreen()
     {
-        if (!CompareLists(_lastTextArray, _textArray)) {
+        if (!CompareLists(_lastTextArray, _textArray))
+        {
             _screenText.text = GridToString(_textArray);
             _lastTextArray = _textArray.ConvertAll(a => new List<char>(a));
         }
@@ -106,14 +120,15 @@ public class Monitor : MonoBehaviour {
         Debug.Log("Current: " + _currentCmd);
 
         if (_textArray.Count.Equals(0))
-        {            
+        {
             _textArray.Add(new List<char>() { val });
             CurrentPosition.x++;
+            CurrentPositionInCmd++;
         }
         else
         {
             _textArray[(int)CurrentPosition.y].Add(val);
-            if (_textArray[(int)CurrentPosition.y].Count.Equals(width - 1))
+            if (_textArray[(int)CurrentPosition.y].Count.Equals(width - 1)) // TODO Needs rewriting for cursor not being at end
             {
                 CurrentPosition = new Vector2(0, CurrentPosition.y + 1);
                 if (_textArray.Count <= CurrentPosition.y)
@@ -124,15 +139,17 @@ public class Monitor : MonoBehaviour {
             else
             {
                 CurrentPosition.x++;
-            }            
+                CurrentPositionInCmd++;
+            }
         }
-    }    
+    }
 
     void ReturnPressed()
-    {        
+    {
         ExecuteCommand();
         _currentCmd = "";
         CurrentPosition = new Vector2(0, CurrentPosition.y + 1);
+        CurrentPositionInCmd = 0;
 
         _textArray.Add(new List<char>());
     }
@@ -145,25 +162,63 @@ public class Monitor : MonoBehaviour {
             _currentCmd = _currentCmd.Substring(0, _currentCmd.Length - 1);
             if (CurrentPosition.x > 0)
             {
-                _textArray[(int)CurrentPosition.y].RemoveAt((int)CurrentPosition.x - 1);                
-                CurrentPosition.x--;
-            } 
+                _textArray[(int)CurrentPosition.y].RemoveAt((int)CurrentPosition.x - 1);
+                CurrentPosition.x--;                
+            }
             else
             {
                 _textArray.RemoveAt((int)CurrentPosition.y);
                 CurrentPosition.y = CurrentPosition.y - 1;
                 CurrentPosition.x = _textArray[(int)CurrentPosition.y].Count - 1;
-                _textArray[(int)CurrentPosition.y].RemoveAt((int)CurrentPosition.x);                
+                _textArray[(int)CurrentPosition.y].RemoveAt((int)CurrentPosition.x);
+            }
+            CurrentPositionInCmd--;
+        }
+    }
+
+    void ArrowPressed(KeyCode arrow)
+    {
+        _cursorScript.ResetTimer();
+        if (arrow == KeyCode.RightArrow)
+        {
+            if (CurrentPositionInCmd < _currentCmd.Length)
+            {
+                CurrentPositionInCmd++;
+                if (CurrentPosition.x < width - 1)
+                {
+                    CurrentPosition.x++;
+                }
+                else
+                {
+                    CurrentPosition.y++;
+                    CurrentPosition.x = 0;
+                }
+            }
+        }
+        else if (arrow == KeyCode.LeftArrow)
+        {
+            if (CurrentPositionInCmd > 0)
+            {
+                CurrentPositionInCmd--;
+                if (CurrentPosition.x > 0)
+                {
+                    CurrentPosition.x--;
+                }
+                else
+                {
+                    CurrentPosition.y--;
+                    CurrentPosition.x = _textArray[(int)CurrentPosition.y].Count - 1;
+                }
             }
         }
     }
-    
+
     void ExecuteCommand()
     {
         Debug.Log("Returned value: " + _currentCmd);
     }
 
-    bool CompareLists<T>(List<List<T>> A, List<List<T>> B) 
+    bool CompareLists<T>(List<List<T>> A, List<List<T>> B)
     {
         if (A.SequenceEqual(B)) return true; // so it doesn't initially keep checking forever when they're empty
         if (!A.Count.Equals(B.Count)) return false;
